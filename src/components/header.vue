e556500fea39175dfb5235af52f5f872e5954df5
gizzhung <mayday0140@gmail.com>
2019-07-23 18:53:08 +0800

    7/23 NewHeader

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,25 +1,65 @@
 <style scoped>
-.header {
+.header_top{
+  height: 80px;
   width: 100%;
-  height: 22vh;
-}
-.head {
-  width: 100%;
-  height: 10vh;
   background-image: url("../assets/header_bg.png");
+  background-repeat: no-repeat; 
+  background-size: Cover ;
+}
+.header_bottom{
+  height: 80px;
+  background-color: #fff;
+  z-index: 999;
+}
+.logo{
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 30px;
+}
+
+.logo img{
+  height:100px;
+}
+
+.nav-item {
+  background-image: url("../assets/menu-item_bg.png");
   background-repeat: no-repeat;
-  background-size: 100vw 100%;
+  background-size: 100% 100%;
+  width: 150px;
+  margin-right: 8px;
+  text-align: center;
 }
-.profile {
-  width: 100%;
-  height: 10vh;
+.menu-link {
+  display: block;
+  font-size: 18px;
+  padding: 10px;
+  font-weight: 700;
+  color: rgba(171, 138, 107, 1);
+  letter-spacing: 10px;
+  text-decoration: none;
+  white-space:nowrap;
 }
-.profile-right {
-  float: right;
-  width: 250px;
-  height: 10vh;
+.navbar-collapse {
+  z-index:5;
 }
-.right{
+.navbar-nav{
+  border-bottom-right-radius:10px;
+  border-bottom-left-radius:10px;  
+}
+.menu-nav {
+  height: 100%;
+}
+
+
+.profile{
+  margin-right: 30px;
+  line-height:  80px;
+}
+.dropdown-item{
+  line-height:  40px;
+}
+.profile-right{
   float: right;
 }
 .profile-name {
@@ -30,238 +70,131 @@
   letter-spacing: 10px;
   text-shadow: 0.1em 0.1em 0.2em black;
 }
-.list {
-  float: right;
-  margin: 0;
-  padding: 1.5rem;
-}
-.list li {
-  display: inline;
-  color: #fff;
-  font-weight: 700;
-  padding-left: 0.5rem;
-  letter-spacing: 10px;
-  text-shadow: 0.1em 0.1em 0.2em black;
-}
-.b-right {
-  border-right: 1.5px solid #fff;
-}
-.mid{
-  padding: .5rem;
-}
-.btn:focus {
-  box-shadow: 0 0 0 0rem rgba(0, 0, 0, 0);
-}
-.menu {
-  width: 100%;
-  min-height: 12vh;
-  background-color: #e7e6e1;
-}
-.logo {
-  z-index: 2;
-  position: absolute;
-  top: 1vh;
-}
-.menu-nav {
-  background-color: #fff;
-  width: 70vw;
-  height: 12vh;
-  float: right;
-}
-.navbar-collapse {
-  z-index:5;
-}
-.navbar-nav {
-  width: 100%;
-  background-color: #fff;
-}
-.nav-item {
-  background-image: url("../assets/menu-item_bg.png");
-  background-repeat: no-repeat;
-  background-size: 100% 100%;
-  width: 150px;
-  margin-right: 2%;  
-}
-.menu-link {
-  display: block;
-  padding: 1rem 0rem 1rem 1.2rem;
-  font-weight: 700;
-  color: rgba(171, 138, 107, 1);
-  letter-spacing: 5px;
-  text-decoration: none;
-  white-space:nowrap;
-}
-.menu-link:hover {
-  text-decoration: none;
-  color: #fff;
-}
-.nav-item:hover {
-  border-radius: 15px;
-  background-color: rgba(171, 138, 107, 1);
-}
-.dropdown-menu {
-  border: 1px solid rgba(171, 138, 107, 1);
-}
-.dropdown-item {
-  letter-spacing: 1px;
-  padding: 0.5rem 1.5rem;
-}
-.dropdown-item:hover {
-  color: #fff;
-  background-color: #ab8a6b;
-  border-bottom: 1px solid rgba(171, 138, 107, 1);
-}
-@media (max-width: 1314px) {
+@media (max-width: 1156px) {
+  .logo{
+    padding: 10px;
+  }
+  .logo img{
+    height:60px;
+  }
+} 
+
+@media (min-width: 992px) {
   .menu-nav {
-    width: 80%;
-  }
-  .menu-link{
-    padding: .5rem;
-  }
-}
-@media (max-width: 1200px) {
-  .menu-nav {
-    width: 85%;
-  }
-  .menu-link{
-    padding: .5rem;
-  }
-}
-@media (max-width: 1200px) {
-  .menu-nav {
-    width: 90%;
-  }
-  .menu-link{
-    padding: .5rem;
-  }
-}
-@media (max-width: 1039px) {
-  .navbar-collapse{
-    width: 93%;
-  }
-  .btn-md{
     float: right;
-    line-height: 100px;
   }
 }
-@media (max-width: 1024px) {
-  .navbar-collapse{
-    width: 95%;
-  }
-  .btn-md{
-    float: right;
-    line-height: 100px;
-  }
-}
+
 @media (max-width: 979px) {
-  .profile-right{
-    font-size: 12px;
-  }
-  .profile-name{
-    font-size: 12px;
-    letter-spacing: 5px;
-  }
-  .btn-md{
-    line-height: 50px;
-  }
-  .logo {
-    top: 0;
-  }
-  .logo img {
-    width: 80%;
-    height: 80%;
-  }
   .menu-nav {
     width: 100%;
+    
   }
-  .menu-link{
-    padding: 1rem;
+  .menu-link:hover{
+    border-radius:10px;
+    background-color: rgba(171, 138, 107, 1);
+    color: #fff;
   }
   .nav-item{
     width: 100%;
     background-image: none;
   }
-}
-@media (max-width: 480px) {
-  .btn-md{
-    line-height: 30px;
+  .navbar-nav{
+    background-color:  #fff;
+    width:200px;
+  }
+  #navbarsExample05{
+     width:200px;
   }
 }
 </style>
 <template>
-    <div class="header">
-      <div class="head">
-          <div class="profile">
-              <div class="profile-right">
-                  <!--未登入前-->
-                  <ul v-if="user == ''" class="list">
-                      <router-link to="/register" class="route_link" style="cursor: pointer;"><li class="b-right">註冊</li></router-link>
-                      <router-link to="/login" class="route_link" style="cursor: pointer;"><li>登入</li></router-link>
-                  </ul>
-                  <!--登入後-->
-                  <div v-else class="dropdown mid">
-                      <button class="btn dropdown-toggle btn-md" type="button" id="dropdownMenu2"
-                          data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          <img src="../assets/user.png" width="32px"><span class="profile-name">{{user.name}}</span>
-                      </button>
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                        <router-link to="/createannounce" v-if="user.status ==4"><button class="dropdown-item" type="button">新增公告</button></router-link>
-                        <router-link to="/profile" ><button class="dropdown-item" type="button">個人資訊</button></router-link>
-                        <router-link to="/resume" ><button class="dropdown-item" type="button">我的履歷</button></router-link>
-                        <router-link to="/signuplist" ><button class="dropdown-item" type="button">我的報名</button></router-link>
-                        <router-link to="/banuser"  v-if="user.status ==4"><button class="dropdown-item" type="button">限制用戶</button></router-link>
-                          <!-- <button class="dropdown-item" type="button">會員專區</button> -->
-                          <div class="dropdown-divider"></div>
-                          <div @click="logout"  class="dropdown-item" style="cursor: pointer;">登出</div>
-
-                      </div>
-                  </div>
+  <div>
+    <div class="header_top">
+      <div class="profile">
+        <div class="profile-right">
+          <!-- 未登入前 -->
+          <ul v-if="user == ''" class="list">
+              <router-link to="/register" class="route_link" style="cursor: pointer;"><li class="b-right">註冊</li></router-link>
+              <router-link to="/login" class="route_link" style="cursor: pointer;"><li>登入</li></router-link>
+          </ul>
+          <!-- 登入後 -->
+          <div v-else class="dropdown mid">
+            <button class="btn dropdown-toggle btn-md" type="button" id="dropdownMenu2"
+                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src="../assets/user.png" width="32px"><span class="profile-name">{{user.name}}</span>
+            </button>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu2">
+              <router-link to="/createannounce" v-if="user.status ==4"><button class="dropdown-item" type="button">新增公告</button></router-link>
+              <router-link to="/profile" ><button class="dropdown-item" type="button">個人資訊</button></router-link>
+              <router-link to="/resume" ><button class="dropdown-item" type="button">我的履歷</button></router-link>
+              <router-link to="/signuplist" ><button class="dropdown-item" type="button">我的報名</button></router-link>
+              <router-link to="/banuser"  v-if="user.status ==4"><button class="dropdown-item" type="button">限制用戶</button></router-link>
+                <button class="dropdown-item" type="button">會員專區</button> 
+                <div class="dropdown-divider"></div>
+                <div @click="logout"  class="dropdown-item" style="cursor: pointer;">登出</div>
               </div>
           </div>
+        </div>
       </div>
-      <div class="logo">
-        <router-link to="/index" style="cursor: pointer;"><img src="../assets/logo.png" height="120" /></router-link>
-      </div>
-      <div class="menu">
-          <nav class="menu-nav navbar navbar-expand-lg navbar-light ">
-              <!--響應式漢堡條-->
-              <button ref="navbar_toggler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05"
-                  aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
+    </div>
+    <div class="header_bottom">
+      <nav class="menu-nav navbar navbar-expand-lg navbar-light ">
+        <button ref="navbar_toggler" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05"
+            aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-              <div class="collapse navbar-collapse" id="navbarsExample05">
-                  <ul class="navbar-nav">
-                      <li class="nav-item" @click="close_nav">
-                          <router-link to="/news" class="menu-link">最新消息</router-link>
-                      </li>
-                      <li class="nav-item dropdown" >
-                          <a class="menu-link dropdown-toggle" href="#" id="dropdown05" data-toggle="dropdown"
-                              aria-haspopup="true" aria-expanded="false">景點簡介</a>
-                          <div class="dropdown-menu" aria-labelledby="dropdown05" @click="close_nav();notOpen()">
-                              <a class="dropdown-item" href="#">第二市場</a>
-                              <a class="dropdown-item" href="#">第三市場</a>
-                              <a class="dropdown-item" href="#">第五市場</a>
-                          </div>
-                      </li>
-                      <li class="nav-item" @click="close_nav">
-                          <router-link to="/photo" class="menu-link">活動花絮</router-link>
-                      </li>
-                      <li class="nav-item" @click="close_nav();notOpen()">
-                          <a class="menu-link" href="#">嚮導簡介</a>
-                      </li>
-                      <li class="nav-item" @click="close_nav">
-                          <router-link to="/Course" class="menu-link">嚮導課程</router-link>
-                      </li>
-                      <li class="nav-item" @click="close_nav();notOpen()">
-                          <a class="menu-link" href="#">導覽媒合</a>
-                      </li>
-                  </ul>
-              </div>
-          </nav>
-      </div>
+        <div class="collapse navbar-collapse" id="navbarsExample05">
+            <ul class="navbar-nav">
+                <li class="nav-item" @click="close_nav">
+                    <router-link to="/news" class="menu-link">最新消息</router-link>
+                </li>
+                <li class="nav-item dropdown" >
+                    <a class="menu-link dropdown-toggle" href="#" id="dropdown05" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">景點簡介</a>
+                    <div class="dropdown-menu" aria-labelledby="dropdown05" @click="close_nav();notOpen()">
+                        <a class="dropdown-item" href="#">第二市場</a>
+                        <a class="dropdown-item" href="#">第三市場</a>
+                        <a class="dropdown-item" href="#">第五市場</a>
+                    </div>
+                </li>
+                <li class="nav-item" @click="close_nav">
+                    <router-link to="/photo" class="menu-link">活動花絮</router-link>
+                </li>
+                <li class="nav-item" @click="close_nav();notOpen()">
+                    <a class="menu-link" href="#">嚮導簡介</a>
+                </li>
+                <li class="nav-item" @click="close_nav">
+                    <router-link to="/Course" class="menu-link">嚮導課程</router-link>
+                </li>
+                <li class="nav-item" @click="close_nav();notOpen()">
+                    <a class="menu-link" href="#">導覽媒合</a>
+                </li>
+            </ul>
+        </div>
+      </nav>
+      
+        <!-- <div class="nav-item">
+          <router-link to="/news" class="menu-link text-center">最新消息</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link to="/news" class="menu-link text-center">最新消息</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link to="/news" class="menu-link text-center">最新消息</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link to="/news" class="menu-link text-center">最新消息</router-link>
+        </div>
+        <div class="nav-item">
+          <router-link to="/news" class="menu-link text-center">最新消息</router-link>
+        </div> -->
+    </div>
+    <div class="logo">
+      <router-link to="/index" style="cursor: pointer;"><img src="../assets/logo.png"  /></router-link>
+    </div>
   </div>
-  
 </template>
 <script>
 import axios from "axios";
